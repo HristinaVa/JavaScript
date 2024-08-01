cfc6371547c27433ee5d153483da48f5b0d98a3b
HristinaVa <83665175+HristinaVa@users.noreply.github.com>
2024-08-01 16:35:50 +0300

    Add files via upload

---
+++ JS DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+let sum1 = document.getElementById('num1');
+let sum2 = document.getElementById('num2');
+let sum =document.getElementById('sum');
+sum.value = Number(sum1.value) + Number(sum2.value);
+}
