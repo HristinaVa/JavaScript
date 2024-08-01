afc7ff598c972cfc820fb83f72a9ced84ce075de
HristinaVa <83665175+HristinaVa@users.noreply.github.com>
2024-08-01 16:36:50 +0300

    Add files via upload

---
+++ JS DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const textElement = document.getElementById('text');
+    const moreElement = document.getElementById('more');
+
+    moreElement.style.display = ('none');
+    textElement.style.display = ('inline');
+}
