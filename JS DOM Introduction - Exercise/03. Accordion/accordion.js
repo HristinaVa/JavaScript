be920306d87febcdbd2bfee975290e9d5d05e98b
HristinaVa <83665175+HristinaVa@users.noreply.github.com>
2024-08-02 16:35:17 +0300

    Add files via upload

---
+++ JS DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -0,0 +1,17 @@
+function toggle() {
+    const buttonEl = document.querySelector('.head span.button');
+    const extraEl = document.getElementById('extra');
+
+    const { display } = extraEl.style;
+    const isHidden = display === 'none';
+
+    if (!display || isHidden) {
+        extraEl.style.display = 'block';
+        buttonEl.textContent = 'Less';
+    }
+    else{
+        extraEl.style.display = 'none';
+        buttonEl.textContent = 'More'; 
+    }
+
+}
